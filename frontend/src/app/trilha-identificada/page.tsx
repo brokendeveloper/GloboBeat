0f9ef02d677b3d89000dcec40ebd90f120668246
Emanuel Henry <emanuelhenrysc@gmail.com>
2025-10-02 19:26:49 -0300

    feat(frontend): Adiciona Componente CardTrilha

--- frontend/src/app/trilha-identificada/page.tsx
+++ frontend/src/app/trilha-identificada/page.tsx
@@ -1,9 +1,53 @@
 import PrincipalHeader from "@/components/PrincipalHeader"
+import CardTrilha from "@/components/CardTrilha"
 
 export default function TrilhaIdentificada(){
+
+    const trilhas = [
+        {
+            nome: "Oceano",
+            album: "Oceano",
+            banda: "Djavan",
+            timeStamp: "00:23 - 1:00",
+            politica: "(Não encontrada/Restrita/Livre)",
+            gMusicID: "XXXXXXXXXX",
+            usada: "2"
+        },
+        {
+            nome: "Oceano",
+            album: "Oceano",
+            banda: "Djavan",
+            timeStamp: "00:23 - 1:00",
+            politica: "(Não encontrada/Restrita/Livre)",
+            gMusicID: "XXXXXXXXXY",
+            usada: "2"
+        },
+        {
+            nome: "Oceano",
+            album: "Oceano",
+            banda: "Djavan",
+            timeStamp: "00:23 - 1:00",
+            politica: "(Não encontrada/Restrita/Livre)",
+            gMusicID: "XXXXXXXXXZ",
+            usada: "2"
+        }
+    ]
+
     return(
         <div className="min-h-screen bg-white">
             <PrincipalHeader/>
+            <main>
+                <div className="flex flex-col items-center !mt-4">
+                    <h1 className="!text-4xl !mb-4 !font-bold text-[#055371]">Trilha(s) identificadas</h1>
+                    {trilhas.map((trilha) => (
+                        <CardTrilha 
+                            nome={trilha.nome} album={trilha.album} banda={trilha.banda}
+                            timestamp={trilha.timeStamp} politica={trilha.politica} gMusicID={trilha.gMusicID}
+                            usada={`${trilha.usada} Vezes`} key={trilha.gMusicID}
+                        />
+                    ))}
+                </div>
+            </main>
         </div>
     )
 }
---
+++ frontend/src/components/CardTrilha.tsx
@@ -0,0 +1,23 @@
+interface CardTrilhaProps{
+    nome: string
+    album: string
+    banda: string
+    timestamp: string
+    politica: string
+    gMusicID: string
+    usada: string
+}
+
+export default function CardTrilha(props:CardTrilhaProps){
+    return(
+        <div className="bg-[#055371] !mb-0.5 w-1/4 h-80 !p-5 text-white !text-2xl rounded-md">
+            <div><strong>Nome:</strong> {props.nome}</div>
+            <div><strong>Albúm:</strong> {props.album}</div>
+            <div><strong>Banda:</strong> {props.banda}</div>
+            <div><strong>Timestamp:</strong> {props.timestamp}</div>
+            <div><strong>Política:</strong> {props.politica}</div>
+            <div><strong>G music ID:</strong> {props.gMusicID}</div>
+            <div><strong>Usada:</strong> {props.usada}</div>
+        </div>
+    )
+}
